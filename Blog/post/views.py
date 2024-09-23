f71016a60d1511dd02ab6dc07b41e37746e63109
Sabrisau <sasaundersvillegas@udc.edu.ar>
2024-09-23 20:07:07 -0300

    Defino views.py

--- Blog/post/views.py
+++ Blog/post/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from .models import Post
+from django.urls import reverse_lazy
 
-# Create your views here.
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'post/post_form.html'
+    success_url = reverse_lazy('post-list')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
