754247536c61dc1b20bc66bc491045d8a2457db9
Sabrisau <sasaundersvillegas@udc.edu.ar>
2024-09-23 20:34:11 -0300

    Defino middleware.py

--- Blog/Blog/urls.py
+++ Blog/Blog/urls.py
@@ -20,7 +20,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('post.urls')),  # Incluye las rutas de la app 'post'
-    path('login/', auth_views.LoginView.as_view(), name='login'),  # Ruta para login
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # Ruta para logout
+    path('posts/', include('post.urls')),  
+    path('login/', auth_views.LoginView.as_view(), name='login'),  
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'), 
 ]
